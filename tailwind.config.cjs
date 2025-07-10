e78120b319d7258d0b9f5c236aacf7e501373e39
davidsanchezroman <129340900+davidsanchezroman@users.noreply.github.com>
2025-07-10 15:02:40 -0400

    ajustes al menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class', // Habilita el modo oscuro basado en la clase 'dark'
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // Habilita el modo oscuro basado en la clase 'dark' en el HTML
   theme: {
     extend: {
       colors: {
         'dark-background': '#1A1A2E',
         'dark-surface': '#252540',
-        'dark-border': '#333350',
+        'dark-border': '#3C3C5C',
         'dark-text': '#E0E0E0',
         'dark-text-muted': '#B0B0B0',
         'accent-blue': '#00ADB5',
         'accent-blue-dark': '#007A80',
-        'accent-purple': '#8E44AD',
-        'accent-purple-dark': '#6C3483',
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], // Usando Inter como ejemplo
-        mono: ['Fira Code', 'monospace'], // Para bloques de código
-      },
+        sans: ['Inter', 'sans-serif'],
+        mono: ['Fira Code', 'monospace'],
+      }
     },
   },
   plugins: [],
