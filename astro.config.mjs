95b3e3762a00b13f3888da1de904ed4e7af090af
davidsanchezroman <129340900+davidsanchezroman@users.noreply.github.com>
2025-07-16 00:28:29 -0400

    chore: add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: netlify({
+    edge: false, // Cambia a true si quieres usar funciones Edge
+  }),
 });
